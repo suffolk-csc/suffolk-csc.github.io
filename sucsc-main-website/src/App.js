52fc46a2f5c301e84c58bfea74e2bc79a3ac7c5a
Sabrina Q. <97366704+squadir10@users.noreply.github.com>
2024-04-03 23:50:04 -0400

    react project made
    Added the react build and all the components necessary to work on the project

---
+++ sucsc-main-website/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// Import your components after you've created them
+// import Header from './components/Header';
+// ... other imports
+
+function App() {
+  return (
+    <div className="App">
+      this is a test pls work
+    </div>
+  );
+}
+
+export default App;
